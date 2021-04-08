7841815b6c5fb7eb3d1c9ab48314631d62ef3aa8
bmtalotta <57196623+bmtalotta@users.noreply.github.com>
2021-04-08 14:15:29 -0800

    ready to test #2

--- CUDA_Minimum_Finding.cu
+++ CUDA_Minimum_Finding.cu
@@ -1,1 +1,45 @@
 #include "stdio.h"
+#include <stdlib.h>
+//based on cuda summing_Arrrays example
+#define N 8 * 1000000
+
+__global__ void findMin(int* a, int low, int high, int minVal)
+{
+    for(int i = low; i < high; i++ ){
+        if(minVal > a[i]){
+            minVal = a[i];
+        }
+    }
+}
+
+int main()
+{
+    dim3 grid(1);
+    dim3 threads(8);
+    int a[N];
+    int *dev_a;
+    cudaMalloc((void**)&dev_a, 8 * sizeof(int));
+    //fill array
+    for (int i = 0; i < N; i++){
+        a[i] = rand() % 1000000000;
+    }
+    cudaMemcpy(dev_a, a, N * sizeof(int), cudaMemcpyHostToDevice);
+    int numToSort = N / 8;
+    int low = numToSort * threadIDx.x;
+    int high = low + numToSort - 1;
+    int MinVal[8];
+    for(int i = 0; i < 8; i++){
+        minVal[i] = 1000000000;
+    }
+    findMin <<<grid, threads >>> (a,low,high,minVal[threadIDx.x]);
+    int minAnswer = MinVal[0];
+    for(int i = 0; i < 8; i++){
+        if(minAnswer > MinVal[i]){
+            minAnswer = minVal[i];
+        }
+    }
+    printf("minimum value using cuda is: %d\n", minAnswer);
+
+    return 0;
+}
+
