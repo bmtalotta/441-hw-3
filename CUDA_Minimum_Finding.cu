5aee9104a8af7def9babd4fb42628ad5265aa295
bmtalotta <57196623+bmtalotta@users.noreply.github.com>
2021-04-08 15:15:32 -0800

    reeeeeeeeeeeee

--- CUDA_Minimum_Finding.cu
+++ CUDA_Minimum_Finding.cu
@@ -2,14 +2,20 @@
 #include <stdlib.h>
 //based on cuda summing_Arrrays example
 #define N 8 * 1000000
-
-__global__ void findMin(int* a, int low, int high, int &minValForThread)
+#define MINE 8
+__global__ void findMin(int* a, int* c )
 {
+    int numToSort = N / 8;
+    int low = numToSort * threadIdx.x;
+    int high = low + numToSort - 1;
+    int minValForThread = 1000000000;
+    
     for(int i = low; i < high; i++ ){
         if(minValForThread > a[i]){
             minValForThread = a[i];
         }
     }
+    c[threadIdx.x] = minValForThread;
 }
 
 int main()
@@ -18,26 +24,26 @@ int main()
     dim3 threads(8);
     int a[N];
     int *dev_a;
-    int minVal[8];
+    int c[8];
+    int *dev_c;
+    
     for(int i = 0; i < 8; i++){
-        minVal[i] = 1000000000;
+        c[i] = 1000000000;
     }
     cudaMalloc((void**)&dev_a, N * sizeof(int));
+    cudaMalloc((void**)&dev_c, 8 * sizeof(int));
     //fill array
     for (int i = 0; i < N; i++){
         a[i] = rand() % 1000000000;
     }
     cudaMemcpy(dev_a, a, N * sizeof(int), cudaMemcpyHostToDevice);
-    int numToSort = N / 8;
-    int low = numToSort * threadIdx.x;
-    int high = low + numToSort - 1;
-    
     findMin <<<grid, threads >>> (a,low,high,minVal[threadIdx.x]);
-    cudaDeviceSynchronize();  
-    int min = minVal[0];
+    
+    cudaMemcpy(c, dev_c, MINE * sizeof(int), cudaMemcpyDeviceToHost);
+    int min = c[0];
     for(int i = 0; i < 8; i++){
-        if(min> minVal[i]){
-            min= minVal[i];
+        if(min> c[i]){
+            min= c[i];
         }
     }
     printf("minimum value using cuda is: %d\n", min);
