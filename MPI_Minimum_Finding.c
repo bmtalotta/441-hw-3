6b5644d47d1a6cacc1df819f307a17d80bc21592
bmtalotta <57196623+bmtalotta@users.noreply.github.com>
2021-04-08 11:41:43 -0800

    fixed more stuff

--- MPI_Minimum_Finding.c
+++ MPI_Minimum_Finding.c
@@ -1,55 +1,75 @@
 #include <mpi.h>
 #include <stdio.h>
+#include <stdlib.h>
 
 #define N 8000000
-int findMin(int a[], int startPoint, int endPoint){
-    int minVal = a[startPoint];
-    for(int i = startPoint; i < endPoint; i++){
-        if(minVal > a[i]){
+int findMin(int a[], int low, int high)
+{
+    int minVal = a[low];
+    for (int i = low; i < high; i++)
+    {
+        if (minVal > a[i])
+        {
             minVal = a[i];
         }
     }
     return minVal;
 }
-int main(int argc, char* argv[]) {
-    int* a, * temp;  
-    int i;   
-    int rank, p;   
-    int tag = 0;   
+
+int main(int argc, char *argv[])
+{
+    int *a, *temp;
+    int i;
+    int rank, p;
+    int tag = 0;
     MPI_Status status;
 
-    MPI_Init(&argc, &argv);   
-    MPI_Comm_rank(MPI_COMM_WORLD, &rank);   
+    MPI_Init(&argc, &argv);
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
     MPI_Comm_size(MPI_COMM_WORLD, &p);
 
-    a = (int*)malloc(sizeof(int) * N);   
-    temp = (int*)malloc(sizeof(int) * 1);
+    a = (int *)malloc(sizeof(int) * N);
+    temp = (int *)malloc(sizeof(int) * 1);
 
-    if (rank == 0) { 
-        for (i = 0; i < N; i++)         
-            a[i] = rand() % 1000000000;    
-    }   
-    MPI_Bcast(a, N, MPI_INT, 0, MPI_COMM_WORLD);
-    int startPoint = 1000000 * rank;
-    int endPoint = startPoint + 1000000 - 1;
-    if(rank != 0){
-        temp[rank] = findMin(a,startPoint,endPoint);
-    MPI_Send(temp, 1, MPI_INT, 0, tag, MPI_COMM_WORLD);
+    if (rank == 0)
+    {
+        for (i = 0; i < N; i++)
+            a[i] = rand() % 1000000000;
     }
-    else{
-        temp[rank] == findMin(a,startPoint,endPoint);
+    MPI_Bcast(a, N, MPI_INT, 0, MPI_COMM_WORLD);
+    int numToSort = N / p;
+    int low = rank * numToSort;
+    int high = low + numToSort - 1;
+
+    if (rank != 0)
+    {
+        temp[0] = findMin(a, low, high);
+        MPI_Send(temp, 1, MPI_INT, 0, tag, MPI_COMM_WORLD);
+    }
+    else
+    {
+        temp[0] == findMin(a, low, high);
     }
     MPI_Barrier(MPI_COMM_WORLD);
     //recieve loop
-    if(rank == 0){
-    for(i = 0; i < 8; i++){
-    MPI_Recv(temp, 1, MPI_INT, i, tag, MPI_COMM_WORLD, &status);
-    }
-    int answer = findMin(temp,0,7);
-    printf("minimum value is: ", answer);
+    if (rank == 0)
+    {
+        int min = temp[0];
+        for (i = 0; i < 8; i++)
+        {
+            MPI_Recv(temp, 1, MPI_INT, i, tag, MPI_COMM_WORLD, &status);
+            if(min > temp[0]){
+                min = temp[0];
+            }
+        }
+        int answer = findMin(a, 0, N-1);
+        printf("minimum value is: ", answer);
     }
     free(a);
     free(temp);
     MPI_finalize();
     return 0;
-    }
+}
+
+
+
