2a11f85b2663515e2396974b0eb5341d354d45ca
bmtalotta <57196623+bmtalotta@users.noreply.github.com>
2021-04-08 11:50:29 -0800

    fixed print messages

--- MPI_Minimum_Finding.c
+++ MPI_Minimum_Finding.c
@@ -63,7 +63,8 @@ int main(int argc, char *argv[])
             }
         }
         int answer = findMin(a, 0, N-1);
-        printf("minimum value is: %d", min);
+        printf("minimum value parallel is: %d", min);
+        printf("minimum value not parallel is: %d", answer);
     }
     free(a);
     free(temp);
