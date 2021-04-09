e944a0c4d62f044eeea4ed6252ba47484c906401
bmtalotta <57196623+bmtalotta@users.noreply.github.com>
2021-04-08 17:45:28 -0800

    Update CUDA_2d_array_sum.cu

--- CUDA_2d_array_sum.cu
+++ CUDA_2d_array_sum.cu
@@ -30,13 +30,6 @@ int main()
     cudaMemcpy(dev_a, a, ROWS * COLUMNS * sizeof(int), cudaMemcpyHostToDevice);
     add <<<1, COLUMNS >>> (dev_a, dev_c);
     cudaMemcpy(c, dev_c, COLUMNS * sizeof(int), cudaMemcpyDeviceToHost);
-
-    for (int y = 0; y < ROWS; y++)              // Output Arrays
-    {
-        for (int x = 0; x < COLUMNS; x++)
-            printf("[%d][%d]=%d ", y, x, c[y][x]);
-        printf("\n");
-    }
     return 0;
 }
 
