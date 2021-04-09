0d00c56dbf662e577039f8a00d9558975123debf
bmtalotta <57196623+bmtalotta@users.noreply.github.com>
2021-04-08 16:58:01 -0800

    asdas

--- CUDA_Minimum_Finding.cu
+++ CUDA_Minimum_Finding.cu
@@ -1,16 +1,17 @@
 #include "stdio.h"
-#include <stdlib.h>
+#include "stdlib.h"
 //based on cuda summing_Arrrays example
-#define N 8 * 1000000
+#define N 8000000
 #define ThreadCount 8
 __global__ void findMin(int* a, int* c )
 {
+
     int numToSort = N / 8;
     int low = numToSort * threadIdx.x;
     int high = low + numToSort - 1;
-    int minValForThread = 1000000000;
-    for(int i = low; i < high; i++){//goes through once doesnt loop
-        printf("here\n");
+    int minValForThread = a[low];
+    for(int i = low; i < high; ++i){//goes through once doesnt loop
+        printf("here in loop\n");
         if(minValForThread > a[i]){
             minValForThread = a[i];
         }
@@ -24,18 +25,18 @@ __global__ void findMin(int* a, int* c )
 int main()
 {
     dim3 grid(1);
-    dim3 threads(ThreadCount);
     int *a;
     a = (int *)malloc(sizeof(int) * N);
     int *dev_a;
     int c[8];
     int *dev_c;
-    
+    cudaMalloc((void**)&dev_a, N * sizeof(int));
+    cudaMalloc((void**)&dev_c, ThreadCount * sizeof(int));
+
     for(int i = 0; i < 8; i++){
         c[i] = 1000000000;
     }
-    cudaMalloc((void**)&dev_a, N * sizeof(int));
-    cudaMalloc((void**)&dev_c, ThreadCount * sizeof(int));
+    
     //fill array
     for (int i = 0; i < N; i++){
         a[i] = rand() % 1000000000;
@@ -43,7 +44,7 @@ int main()
     
     cudaMemcpy(dev_a, a, N * sizeof(int), cudaMemcpyHostToDevice);
     cudaMemcpy(dev_c, c, ThreadCount * sizeof(int), cudaMemcpyHostToDevice);
-    findMin <<<grid, threads >>> (a, c);
+    findMin <<<grid, ThreadCount >>> (dev_a, dev_c);
 
     cudaMemcpy(c, dev_c, ThreadCount * sizeof(int), cudaMemcpyDeviceToHost);
     int min = c[0];
