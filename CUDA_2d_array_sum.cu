b942d7f04433eb779235b194743a814bcaf85f36
bmtalotta <57196623+bmtalotta@users.noreply.github.com>
2021-04-08 18:03:06 -0800

    fixed a typo

--- CUDA_2d_array_sum.cu
+++ CUDA_2d_array_sum.cu
@@ -32,7 +32,7 @@ int main()
     cudaMemcpy(dev_a, a, ROWS * COLUMNS * sizeof(int), cudaMemcpyHostToDevice);
     cudaMemcpy(dev_c, c, COLUMNS * sizeof(int), cudaMemcpyHostToDevice);
     add <<<1, COLUMNS >>> (dev_a, dev_c);
-    cudaMemcpy(c, dev_c, COLUMNS * sizeof(int), cudaMemcpyHostToDevice);
+    cudaMemcpy(c, dev_c, COLUMNS * sizeof(int), cudaMemcpyDeviceToHost);
     cudaDeviceSynchronize();//wait for threads to finish
     int findColSum = 0;
     for(int i = 0; i < COLUMNS; i++){
